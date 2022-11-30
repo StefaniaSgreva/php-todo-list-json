b097c0769acd94e5364297110f1252109cf5167e
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-30 23:59:43 +0100

    bugs fixed

--- JS/script.js
+++ JS/script.js
@@ -12,29 +12,31 @@ createApp({
   methods:{
     getTodo(){
         axios.get(this.apiUrl).then((response)=>{
-            // console.log(response);
+            // console.log(response.data);
             this.todoList = response.data;
         });
     },
     addTodo(){
         const data = {
-            //nome : valore della chiave
+            //nome della chiave : valore della chiave
             newTodoText: this.newTodoText,
         }
-        //headers per ingannare il server e farli credere che è un form
+        
         axios.post(
             this.apiUrl, 
             data,
-            {headrs: {'Content-Type': 'multipart/form-data'}}
+            //headers per ingannare il server e fargli credere che è un form
+            {headers: {'Content-Type': 'multipart/form-data'}}
             ).then((response)=>{
                 console.log(response.data);
 
+                //pre-eseguiamo la funzione che ci carica i todo in pagina (anche se non è stato aggiunto niente)
                 this.getTodo();
 
                 this.newTodoText = '';
 
             });
-    }
+    },
   },
   mounted(){
     this.getTodo();
--- server.php
+++ server.php
@@ -2,28 +2,37 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
-$file_url = './data.json';
+$file_url = './data.json'; //creo variabile per utilizzarla dove mi serve sotto
 
 //leggo il file di testo (_text)
 $file_text = file_get_contents($file_url);
+
 //converto il testo json in un array php
 $todo_list = json_decode($file_text);
 
 if (isset($_POST['newTodoText'])) {
 
-    // echo 'il parametro è arrivato';
+    // echo 'il parametro post è arrivato';
+
+    //prendere il valore inviatoci
+    //inserirlo nell'array
+    //salvarlo nel file
+    //ricaricare i nuovi todo
+
     $newTodo = [
         'text' => $_POST['newTodoText'],
         'done' => false,
     ];
 
     array_push($todo_list, $newTodo);
+    // print_r($todo_list);
+    file_put_contents($file_url, json_encode($todo_list));
 
-    file_put_contents($file_url, jason_encode($todo_list));
 
 } else {
 
-    // echo 'il parametro non è arrivato';ù
+    // echo 'il parametro post non è arrivato';
     header('Content-Type: application/json');
     echo json_encode($todo_list);
+
 }
